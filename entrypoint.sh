4270166c5c5aa1b4ca7dfac0961c45b48b36b3b9
majamee <majamee@yandex.com>
2017-12-18 12:33:37 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -18,7 +18,7 @@ do
             fi
             
             rm -rf "/tmp/video/*";
-            ffmpeg -y -threads 4 -i "${file}" -an -c:v libx264 -x264opts 'keyint=24:min-keyint=24:no-scenecut' -profile:v high -level 4.0 -vf "scale=min'(1920,iw)':-4" -crf 22 -movflags faststart -write_tmcd 0 "/tmp/video/${file}" && \ 
+            ffmpeg -y -threads 4 -i "${file}" -an -c:v libx264 -x264opts 'keyint=24:min-keyint=24:no-scenecut' -profile:v high -level 4.0 -vf "scale=min'(1920,iw)':-4" -crf 22 -movflags faststart -write_tmcd 0 "/tmp/video/${file}"; 
             if [ ${PIPESTATUS[0]} -eq 0 ]; then
                 oldsize=$(wc -c <"${file}");
                 newsize=$(wc -c <"/tmp/video/${file}");
