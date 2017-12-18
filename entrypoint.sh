f8fe5900134e13b7c614183cb3995221fa2cc7f2
majamee <majamee@yandex.com>
2017-12-18 12:58:09 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@ mkdir -p "/tmp/video/";
 shopt -s nullglob;
 
 echo "Please be aware, that the audio track of all videos in provided folder will be cut. Due to that, originals will be kept renamed.";
-echo "Processing ${1}";
+echo -e "\n\e[5mProcessing ${1}\n";
 cd /video/$1;
 for file in *
 do
@@ -13,7 +13,7 @@ do
         if ( [ ${file: -4} == ".avi" ] || [ ${file: -4} == ".mkv" ] || [ ${file: -4} == ".mp4" ] || [ ${file: -4} == ".wmv" ] || [ ${file: -4} == ".ts" ] || [ ${file: -4} == ".mov" ] || [ ${file: -4} == ".flv" ] || [ ${file: -4} == ".webm" ] ); then
             if grep -Fxq "${file}" /video/.hero-videoptim
             then
-                echo "${file} already optimized in previous run. Skipping";
+                echo -e "\e[104m${file} already optimized in previous run. Skipping";
                 continue
             fi
             
@@ -28,13 +28,14 @@ do
                     mv "${file}" "${file}.backup";
                     mv "/tmp/video/${file}" "${file}";
                 else
-                    echo "Optimized file is not smaller. Skipping";
+                    echo -e "\e[41mOptimized file for ${file} is not smaller. Skipping";
                 fi
             else
-                echo "Optimizing file ${file} failed. Skipping";
+                echo -e "\e[101mOptimizing file ${file} failed. Skipping";
             fi
             
             echo "${file}" >> /video/.hero-videoptim;
+            echo -e "\e[42mOptimized file \e[1m${file} \e[21msuccessfully as hero-video";
         fi
     fi
 done
